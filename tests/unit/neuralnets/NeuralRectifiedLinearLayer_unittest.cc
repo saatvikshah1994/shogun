6ed1190b0e5949aa16634c1d1a18814ec80c93a6
saatviks <saatviks@andrew.cmu.edu>
2018-11-25 11:02:06 -0500

    Fix Neural*Layer unit-test redundancies

--- tests/unit/neuralnets/NeuralRectifiedLinearLayer_unittest.cc
+++ tests/unit/neuralnets/NeuralRectifiedLinearLayer_unittest.cc
@@ -31,47 +31,79 @@
  * Written (W) 2014 Khaled Nasr
  */
 
-#include <shogun/neuralnets/NeuralRectifiedLinearLayer.h>
-#include <shogun/neuralnets/NeuralInputLayer.h>
-#include <shogun/lib/SGVector.h>
-#include <shogun/lib/SGMatrix.h>
-#include <shogun/mathematics/Math.h>
-#include <gtest/gtest.h>
+#include "shogun/neuralnets/NeuralRectifiedLinearLayer.h"
+#include "shogun/neuralnets/NeuralInputLayer.h"
+#include "shogun/lib/SGVector.h"
+#include "shogun/lib/SGMatrix.h"
+#include "shogun/mathematics/Math.h"
+#include "gtest/gtest.h"
+
+#include <tuple>
+#include <memory>
 
 using namespace shogun;
+
+class NeuralRectifiedLinearLayerTest  : public ::testing::Test {
+ public:
+
+  template <typename T>
+  SGMatrix<T> CreateRandSGMat(int32_t num_features, int32_t num_samples,
+  		T lower_bound, T upper_bound) {
+	  auto data_batch = SGMatrix<T>(num_features, num_samples);
+	  for (int32_t i=0; i<data_batch.num_rows*data_batch.num_cols; i++) {
+		  data_batch[i] = CMath::random(lower_bound, upper_bound);
+	  }
+	  return data_batch;
+  }
+
+  template <typename T>
+  auto CreateRandSGMatAndInputLayer(
+  		int32_t num_features, int32_t num_samples,
+  		T lower_bound, T upper_bound) {
+	  auto data_batch = CreateRandSGMat(num_features, num_samples, lower_bound, upper_bound);
+	  // Can't use unique_ptr here due to "unref_all"
+	  auto input_layer = new CNeuralInputLayer(data_batch.num_rows);
+	  input_layer->set_batch_size(data_batch.num_cols);
+	  input_layer->compute_activations(data_batch);
+	  return std::make_tuple(data_batch, input_layer);
+  }
+
+  auto InitNeuralLinearLayer(CNeuralLinearLayer* layer,
+  		CDynamicObjectArray* layers, SGVector<int32_t> input_indices,
+  		int32_t batch_size, double sigma) {
+	  layer->initialize_neural_layer(layers, input_indices);
+	  SGVector<float64_t> params(layer->get_num_parameters());
+	  SGVector<bool> param_regularizable(layer->get_num_parameters());
+	  layer->initialize_parameters(params, param_regularizable, sigma);
+	  layer->set_batch_size(batch_size);
+	  layer->compute_activations(params, layers);
+	  return params;
+  }
+ protected:
+  void SetUp() {
+	  CMath::init_random(100);
+	  m_layers = std::make_unique<CDynamicObjectArray>();
+  }
+  std::unique_ptr<CDynamicObjectArray> m_layers;
+};
 
 /** Compares the activations computed using the layer against manually computed
  * activations
  */
-TEST(NeuralRectifiedLinearLayer, compute_activations)
+TEST_F(NeuralRectifiedLinearLayerTest, compute_activations)
 {
-	CNeuralRectifiedLinearLayer layer(9);
 
 	// initialize some random inputs
-	CMath::init_random(100);
-	SGMatrix<float64_t> x(12,3);
-	for (int32_t i=0; i<x.num_rows*x.num_cols; i++)
-		x[i] = CMath::random(-10.0,10.0);
-
-	CNeuralInputLayer* input = new CNeuralInputLayer (x.num_rows);
-	input->set_batch_size(x.num_cols);
-
-	CDynamicObjectArray* layers = new CDynamicObjectArray();
-	layers->append_element(input);
-
-	SGVector<int32_t> input_indices(1);
-	input_indices[0] = 0;
+	SGMatrix<float64_t> x;
+	CNeuralInputLayer* input;
+	std::tie(x, input) = CreateRandSGMatAndInputLayer<float64_t>(12, 3, -10.0, 10.0);
+	m_layers->append_element(input);
 
 	// initialize the layer
-	layer.initialize_neural_layer(layers, input_indices);
-	SGVector<float64_t> params(layer.get_num_parameters());
-	SGVector<bool> param_regularizable(layer.get_num_parameters());
-	layer.initialize_parameters(params, param_regularizable, 1.0);
-	layer.set_batch_size(x.num_cols);
-
-	// compute the layer's activations
-	input->compute_activations(x);
-	layer.compute_activations(params, layers);
+	CNeuralRectifiedLinearLayer layer(9);
+	SGVector<int32_t> input_indices(1);
+	input_indices[0] = 0;
+	auto params = InitNeuralLinearLayer(&layer, m_layers.get(), input_indices, x.num_cols, 1.0);
 	SGMatrix<float64_t> A = layer.get_activations();
 
 	// manually compute the layer's activations
@@ -98,78 +130,45 @@ TEST(NeuralRectifiedLinearLayer, compute_activations)
 	EXPECT_EQ(A_ref.num_cols, A.num_cols);
 	for (int32_t i=0; i<A.num_rows*A.num_cols; i++)
 		EXPECT_NEAR(A_ref[i], A[i], 1e-12);
-
-	SG_UNREF(layers);
 }
 
 /** Compares the parameter gradients computed using the layer, when the layer
  * is used as a hidden layer, against gradients computed using numerical
  * approximation
  */
-TEST(NeuralRectifiedLinearLayer, compute_parameter_gradients_hidden)
-{
-	SGMatrix<float64_t> x1(12,3);
-	for (int32_t i=0; i<x1.num_rows*x1.num_cols; i++)
-		x1[i] = CMath::random(-10.0,10.0);
+TEST_F(NeuralRectifiedLinearLayerTest, compute_parameter_gradients_hidden) {
+	
+	// initialize some random inputs
+	SGMatrix<float64_t> x1, x2;
+	CNeuralInputLayer *input1, *input2;
+	std::tie(x1, input1) = CreateRandSGMatAndInputLayer<float64_t>(12, 3, -10.0, 10.0);
+	std::tie(x2, input2) = CreateRandSGMatAndInputLayer<float64_t>(7, 3, -10.0, 10.0);
+	m_layers->append_element(input1);
+	m_layers->append_element(input2);
 
-	CNeuralInputLayer* input1 = new CNeuralInputLayer (x1.num_rows);
-	input1->set_batch_size(x1.num_cols);
-
-	SGMatrix<float64_t> x2(7,3);
-	for (int32_t i=0; i<x2.num_rows*x2.num_cols; i++)
-		x2[i] = CMath::random(-10.0,10.0);
-
-	CNeuralInputLayer* input2 = new CNeuralInputLayer (x2.num_rows);
-	input2->set_batch_size(x2.num_cols);
-
-	// initialize hidden the layer
+	// initialize the hidden layer
 	CNeuralLinearLayer* layer_hid = new CNeuralRectifiedLinearLayer(5);
-
-	CDynamicObjectArray* layers = new CDynamicObjectArray();
-	layers->append_element(input1);
-	layers->append_element(input2);
-	layers->append_element(layer_hid);
-
+	m_layers->append_element(layer_hid);
 	SGVector<int32_t> input_indices_hid(2);
 	input_indices_hid[0] = 0;
 	input_indices_hid[1] = 1;
-
-	SGVector<int32_t> input_indices_out(1);
-	input_indices_out[0] = 2;
-
-	SGMatrix<float64_t> y(9,3);
-	for (int32_t i=0; i<y.num_rows*y.num_cols; i++)
-		y[i] = CMath::random(0.0,1.0);
-
-	// initialize the hidden layer
-	layer_hid->initialize_neural_layer(layers, input_indices_hid);
-	SGVector<float64_t> param_hid(layer_hid->get_num_parameters());
-	SGVector<bool> param_regularizable_hid(layer_hid->get_num_parameters());
-	layer_hid->initialize_parameters(param_hid, param_regularizable_hid, 0.01);
-	layer_hid->set_batch_size(x1.num_cols);
+	auto param_hid = InitNeuralLinearLayer(layer_hid, m_layers.get(), input_indices_hid, x1.num_cols, 0.01);
 
 	// initialize the output layer
+	auto y = CreateRandSGMat(9, 3, 1.0, 0.0);
 	CNeuralLinearLayer layer_out(y.num_rows);
-	layer_out.initialize_neural_layer(layers, input_indices_out);
-	SGVector<float64_t> param_out(layer_out.get_num_parameters());
-	SGVector<bool> param_regularizable_out(layer_out.get_num_parameters());
-	layer_out.initialize_parameters(param_out, param_regularizable_out, 0.01);
-	layer_out.set_batch_size(x1.num_cols);
-
-	// compute activations
-	input1->compute_activations(x1);
-	input2->compute_activations(x2);
-	layer_hid->compute_activations(param_hid, layers);
-	layer_out.compute_activations(param_out, layers);
+	SGVector<int32_t> input_indices_out(1);
+	input_indices_out[0] = 2;
+	auto param_out = InitNeuralLinearLayer(&layer_out, m_layers.get(), input_indices_out, x1.num_cols, 0.01);
 
 	// compute gradients
 	layer_hid->get_activation_gradients().zero();
 	SGVector<float64_t> gradients_out(layer_out.get_num_parameters());
-	layer_out.compute_gradients(param_out, y, layers, gradients_out);
+	layer_out.compute_gradients(param_out, y, m_layers.get(), gradients_out);
 
 	SGVector<float64_t> gradients_hid(layer_hid->get_num_parameters());
 	layer_hid->compute_gradients(param_hid, SGMatrix<float64_t>(),
-			layers, gradients_hid);
+			m_layers.get(), gradients_hid);
 
 	// manually compute parameter gradients
 	SGVector<float64_t> gradients_hid_numerical(layer_hid->get_num_parameters());
@@ -179,15 +178,15 @@ TEST(NeuralRectifiedLinearLayer, compute_parameter_gradients_hidden)
 		param_hid[i] += epsilon;
 		input1->compute_activations(x1);
 		input2->compute_activations(x2);
-		layer_hid->compute_activations(param_hid, layers);
-		layer_out.compute_activations(param_out, layers);
+		layer_hid->compute_activations(param_hid, m_layers.get());
+		layer_out.compute_activations(param_out, m_layers.get());
 		float64_t error_plus = layer_out.compute_error(y);
 
 		param_hid[i] -= 2*epsilon;
 		input1->compute_activations(x1);
 		input2->compute_activations(x2);
-		layer_hid->compute_activations(param_hid, layers);
-		layer_out.compute_activations(param_out, layers);
+		layer_hid->compute_activations(param_hid, m_layers.get());
+		layer_out.compute_activations(param_out, m_layers.get());
 		float64_t error_minus = layer_out.compute_error(y);
 		param_hid[i] += epsilon;
 
@@ -197,6 +196,4 @@ TEST(NeuralRectifiedLinearLayer, compute_parameter_gradients_hidden)
 	// compare
 	for (int32_t i=0; i<gradients_hid_numerical.vlen; i++)
 		EXPECT_NEAR(gradients_hid_numerical[i], gradients_hid[i], 1e-6);
-
-	SG_UNREF(layers);
 }
